be8f1ae662c4bb45685c146d212dc7b3ab615921
m.lami <Mohammad.lami57@gmail.com>
2022-12-18 16:50:02 +0330

    remove Repository classes

--- src/main/java/com/core/framework/repository/IGenericRepository.java
+++ src/main/java/com/core/framework/repository/IGenericRepository.java
@@ -5,14 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import java.io.Serializable;
 
 public interface IGenericRepository<T, PK extends Serializable> extends CrudRepository<T, PK> {
-   /* T load(PK entityId);
-
-    List<T> getAll();
-
-    PK add(T t);
-
-    void update(T t);
-
-    void deleteById(PK entityId);*/
-
 }
