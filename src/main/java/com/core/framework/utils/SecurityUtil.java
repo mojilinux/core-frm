cf82e7ee0e26785e02ff165fd0c4bb9f09d112d1
Behzad <mohammad.lami57@gmail.com>
2022-12-19 14:30:56 +0330

    add get authenticatedUser in SecUtil

--- src/main/java/com/core/framework/utils/SecurityUtil.java
+++ src/main/java/com/core/framework/utils/SecurityUtil.java
@@ -1,8 +1,12 @@
 package com.core.framework.utils;
 
 import com.core.framework.domain.user.User;
+import com.core.framework.domain.user.UserPrincipal;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
 
 public class SecurityUtil {
 
@@ -15,5 +19,11 @@ public class SecurityUtil {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		User principal = (User) authentication.getPrincipal();
 		return principal.getId();
+	}
+
+	public static Collection<? extends GrantedAuthority> getAuthenticatedUserAuthorities() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+		return principal.getAuthorities();
 	}
 }
